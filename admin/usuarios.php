001204398ccc670cae91b4418580f22f8ad2764f
johelmejias <jomejiasma@est.utn.ac.cr>
2025-08-22 18:45:00 -0600

    Creación básica para administrar usuario
    Crear, editar y eliminar usuario.

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,6 +1,27 @@
 <?php
 include '../config/conexion.php';
 include '../config/consultasDB.php';
+include '../includes/seguridad.php';
+include '../includes/alert.php';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['registro']) && $_POST['registro'] == 1) {
+    $nombre = $_POST['nombre'];
+    $telefono = $_POST['telefono'];
+    $email = $_POST['email'];
+    $user = $_POST['user'];
+    $pass = encryptPassword($_POST['user']);
+    $rol = $_POST['rol'];
+    $conn = conectar();
+    
+    if(insertUsuario($conn, $nombre, $telefono, $email, $user, $pass, $rol, 1)){
+        alertMenssage("Usuario registrado exitosamente", "success");
+    } else {
+        alertMenssage("Error al registrar el usuario", "danger");
+    }
+    $_POST['registro'] = 0;
+    $conn->close();
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,16 +30,17 @@ include '../config/consultasDB.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inmobiliaria</title>
+    <link rel="stylesheet" href="../css/alert.css">
 </head>
 
 <body>
     <div id="container">
-        <button><a href="index.php">Volver</a></button>
+        <button><a href="../index.php">Volver</a></button>
         <section class="bloque">
-            <form action="ingresarProfesor.php" method="post" enctype="multipart/form-data">
+            <form action="" method="post">
                 <h4>Ingresar Usuario</h4>
-                <label for="profesor">Nombre Completo:</label>
-                <input type="text" name="profesor" id="profesor" required>
+                <label for="nombre">Nombre Completo:</label>
+                <input type="text" name="nombre" id="nombre" required>
                 <label for="telefono">Telefono:</label>
                 <input type="text" name="telefono" id="telefono" pattern="^[0-9]{8,12}$" required>
                 <label for="email">Email:</label>
@@ -42,11 +64,51 @@ include '../config/consultasDB.php';
                     $conn->close();
                     ?>
                 </select>
-
-                <button type="submit"> Registrar</button>
+                <button type="submit" value="1" name="registro"> Registrar</button>
             </form>
         </section>
-    </div>
-</body>
 
+        <section class="bloque">
+            <?php
+            $conn = conectar();
+            $resultado = getUsuarios($conn);
+            if ($resultado->num_rows > 0):?>
+            <h4>Lista de usuarios</h4>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Teléfono</th>
+                        <th>Email</th>
+                        <th>Usuario</th>
+                        <th>Privilegio</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $resultado->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['nombre']; ?></td>
+                        <td><?php echo $row['telefono']; ?></td>
+                        <td><?php echo $row['email']; ?></td>
+                        <td><?php echo $row['usuario']; ?></td>
+                        <td><?php echo $row['privilegio_nombre']; ?></td>
+                        <td>
+                            <a href="./editarUsuario.php?id=<?php echo $row['id'] ?>">Editar</a> |
+                            <a href="./eliminarUsuario.php?id=<?php echo $row['id'] ?>" onclick="return confirm('¿Estás seguro de eliminar este usuario?');">Eliminar</a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                </tbody>
+
+            <?php endif;?>
+        </section>
+    </div>
+
+    <script>
+
+    </script>
+</body>
 </html>
